aacb2b69e7be6755cf6ac0b6d5b0a18b4116c1f9
Syed Aanif <syedaanif07@gmail.com>
2024-09-08 09:29:52 +0530

    intro to react

---
+++ 01-react-intro/components/Navbar.jsx
@@ -0,0 +1,12 @@
+// after instal ES7 extension, just type rafce for biolerplate code
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+      <h2>This is the Navbar Component</h2>
+    </div>
+  )
+}
+
+export default Navbar
---
+++ 01-react-intro/src/App.jsx
@@ -0,0 +1,13 @@
+import Navbar from "../components/Navbar";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <h1>Hello! First React App</h1>
+    </>
+  );
+}
+
+export default App;
